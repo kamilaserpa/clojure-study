be663e44b38bbc083691205876c463e307fce3dd
kamila.serpa <kamilardg@gmail.com>
2022-09-09 16:51:56 -0300

    3.5 Transferindo pessoas no hospital e muito threading

---
+++ hospital/src/hospital/aula5.clj
@@ -0,0 +1,25 @@
+(ns hospital.aula5
+  (:use [clojure pprint])
+  (:require [hospital.logic :as h.logic]
+            [hospital.model :as h.model]))
+
+; Quem chega ao hospital chega sempre na fila de Espera
+(defn chega-em! [hospital pessoa]
+  (swap! hospital h.logic/chega-em :espera pessoa))
+
+; Remove o paciente
+(defn transfere! [hospital de para]
+  (swap! hospital h.logic/transfere de para))
+
+(defn simula-um-dia []
+  (let [hospital (atom (h.model/novo-hospital))]
+    (chega-em! hospital "João")
+    (chega-em! hospital "Maria")
+    (chega-em! hospital "Daniela")
+    (chega-em! hospital "Guilherme")
+
+    (transfere! hospital :espera :laboratorio1)
+    (pprint hospital)))
+
+(simula-um-dia)
+
--- hospital/src/hospital/logic.clj
+++ hospital/src/hospital/logic.clj
@@ -2,7 +2,7 @@
 ; COnsideramos que no hospital há um limite de 5 pessoas por fila
 
 
-(defn cheio? [hospital departamento]
+(defn cabe-na-fila? [hospital departamento]
   (-> hospital                                              ; thread first pois envia o argumento como primeiro parâmetro nas chamadas abaixo
       (get,,, departamento)                                 ; obtem departamento para o hospital
       count,,,                                              ; conta índices do departamento
@@ -12,7 +12,7 @@
 ; Adiciona paciente ao final de uma fila de departamento
 (defn chega-em
   [hospital departamento pessoa]
-  (if (cheio? hospital departamento)
+  (if (cabe-na-fila? hospital departamento)
     (update hospital departamento conj pessoa)              ; atualiza o hospital, adiciona (conj) a pessoa no departamento (LaboratorioN)
     (throw (ex-info "Fila já está cheia!" {:tentando-adicionar pessoa})))) ; lançando exception
 
@@ -22,24 +22,43 @@
   [hospital departamento]
   (update hospital departamento pop))
 
+
+(defn proxima [hospital departamento]
+  "Retorna o próximo (primeiro) paciente da fila de um departamento"
+  (-> hospital
+      departamento
+      peek))
+
+; Remove o primeiro paciente da fila de espera e adiciona na fila de um laboratório
+(defn transfere [hospital departamento-origem departamento-destino]
+  (let [pessoa (proxima hospital departamento-origem)]
+    (-> hospital
+        (atende departamento-origem)
+        (chega-em departamento-destino pessoa))))
+
+
+
+
+
 ; Adiciona paciente ao final de uma fila de departamento
 ; Simulando vários processamentos em paralelo no servidor
 ; Erro de imprevisibilidade da ordem de execução
 ; Função malvada não pura pois utiliza randon trazendo comportamentos diferentes para cada chamada
+#_
 (defn chega-em-pausado
   [hospital departamento pessoa]
   (Thread/sleep (* (rand) 1000))
-  (if (cheio? hospital departamento)
+  (if (cabe-na-fila? hospital departamento)
     (do                                                     ;(Thread/sleep (* (rand) 1000))                                 ; utiliza-se 'do' para caos true com mais de um bloco
       (update hospital departamento conj pessoa))
     (throw (ex-info "Fila já está cheia!" {:tentando-adicionar pessoa}))))
 
-
+#_
 (defn chega-em-pausado-logando
   [hospital departamento pessoa]
   (println "Tentando adicionar a pessoa" pessoa)
   (Thread/sleep (* (rand) 1000))
-  (if (cheio? hospital departamento)
+  (if (cabe-na-fila? hospital departamento)
     (do (println "Dei o update:" pessoa)
         (update hospital departamento conj pessoa))
     (throw (ex-info "Fila já está cheia!" {:tentando-adicionar pessoa}))))
